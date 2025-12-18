ecd0625763aedf0b3da647b9d5201b9011171a46
Swastika Roy <swastikaroytitu@gmail.com>
2025-12-18 15:22:21 +0530

    logical and relational operators in kotlin

---
+++ src/Basics/operators/RelationalOperator.kt
@@ -0,0 +1,11 @@
+package Basics.operators
+
+fun main(){
+    var a = 55;
+    var b = 65;
+    if(a.compareTo(b)>0){
+        println("a > b");
+    }else if(a.compareTo(b)<0){
+        println("b>a");
+    }
+}
---
+++ src/Basics/operators/logicalOperator.kt
@@ -0,0 +1,11 @@
+package Basics.operators
+
+fun main(){
+    var year = 2000;
+    if(year.rem(400)==0 && year.rem(4)==0 || year.rem(100) != 0) {
+        println("yes ${year} is leap year.")
+    };
+    else{
+        println("No ${year} is not a leap year.")
+    }
+}
