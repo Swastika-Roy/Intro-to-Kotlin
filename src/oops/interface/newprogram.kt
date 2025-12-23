de58bb657b161f0d7b40f0131790519c0cb19eb7
Swastika Roy <swastikaroytitu@gmail.com>
2025-12-23 20:08:02 +0530

    done

--- src/oops/interface/newprogram.kt
+++ src/oops/interface/newprogram.kt
@@ -21,10 +21,23 @@ class Rectangle(override val length: Double, override val breadth: Double) : dim
 
 }
 
+class Square(override val length : Double, override val breadth: Double ): dimensions, calculation{
+    override fun area() : Double{
+      return length*length;
+    }
+
+    override fun perimeter(): Double {
+        return length*4;
+    }
+}
+
 fun main(){
     var obj = Rectangle(10.00,10.00)
+    var obj2 = Square(10.00,10.00)
     obj.area()
     obj.perimeter()
     println("area = "+ obj.area())
     println("perimeter = "+obj.perimeter())
+    println("area = "+ obj2.area())
+    println("perimeter = "+obj2.perimeter())
 }
